c509c2b8d1b0a16d7f594105b9285e2cc734960d
Piotr Suwara <piotrsuw03@gmail.com>
2024-05-01 21:09:33 +0200

    ProjektFinito

---
+++ WebApplication1/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DefaultNamespace;
+
+public class Product
+{
+    [Required]
+    public int IdProduct { get; set; }
+    [Required]
+    [MaxLength(200)]
+    public string Name { get; set; }
+    [Required]
+    [MaxLength(200)]
+    public string Description { get; set; }
+    [Required]
+    [MaxLength(25)]
+    public decimal Price { get; set; }
+}
---
+++ WebApplication1/Models/Warehouse.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DefaultNamespace;
+
+public class Warehouse
+{
+    [Required]
+    public int IdWarehouse { get; set; }
+    [Required]
+    [MaxLength(200)]
+    public string Name { get; set; }
+    [Required]
+    [MaxLength(200)]
+    public string Address { get; set; }
+    
+}
